0705cfe7d052402b1b4efbb04bafb0031e327e6e
Felipe Ferreira de Paula <felipefp2@gmail.com>
2023-04-28 13:42:55 -0300

    Ajuste Idade 1

--- JavaScript/projetoIdade/idade.js
+++ JavaScript/projetoIdade/idade.js
@@ -19,7 +19,7 @@ function verificar() {
             if (idade <= 5) {
                 img.src = "imagens/bebe.jpeg"
                 res.innerHTML = "Nenê " + idade + " anos.";
-                backG.style.background = "rgba(44, 211, 94, 0.815)"
+                backG.style.background = "rgba(44, 211, 94, 0.8)"
             }else if (idade > 5 && idade <= 17) {
                 img.src = "imagens/criancaMenino.jpeg"
                 res.innerHTML = "Homem de " + idade + " anos.";
@@ -38,19 +38,19 @@ function verificar() {
             if (idade < 5) {
                 img.src = "imagens/bebe.jpeg"
                 res.innerHTML = "Nenê " + idade + " anos.";
-                backG.style.background = "rgba(44, 211, 94, 0.815)"
+                backG.style.background = "rgba(44, 211, 94, 0.8)"
             }else if (idade > 5 && idade <= 17) {
                 img.src = "imagens/criancaMenina.jpeg"
                 res.innerHTML = "Mulher de " + idade + " anos.";
-                backG.style.background = "rgba(176, 78, 196, 0.815)"
+                backG.style.background = "rgba(176, 78, 196, 0.8)"
             }else if (idade > 17 && idade <= 50) {
                 img.src = "imagens/meiaIdadeMulher.jpeg"
                 res.innerHTML = "Mulher de " + idade + " anos.";
-                backG.style.background = "rgba(176, 78, 196, 0.815)"
+                backG.style.background = "rgba(176, 78, 196, 0.8)"
             }else if (idade > 50) {
                 img.src = "imagens/idosaMulher.jpeg"
                 res.innerHTML = "Mulher de " + idade + " anos.";
-                backG.style.background = "rgba(176, 78, 196, 0.815)"
+                backG.style.background = "rgba(176, 78, 196, 0.8)"
             }
         }
     }
