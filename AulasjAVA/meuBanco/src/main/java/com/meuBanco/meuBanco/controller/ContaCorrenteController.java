ddd8c31ad3f9fc725534148d69e662a92446b50f
Felipe Ferreira de Paula <felipefp2@gmail.com>
2023-05-09 21:21:46 -0300

    1Teste meuBanco

--- AulasjAVA/meuBanco/src/main/java/com/meuBanco/meuBanco/controller/ContaCorrenteController.java
+++ AulasjAVA/meuBanco/src/main/java/com/meuBanco/meuBanco/controller/ContaCorrenteController.java
@@ -1,4 +1,19 @@
 package com.meuBanco.meuBanco.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContaCorrenteController {
+
+    @GetMapping("/contas")
+    public String consultarSaldo(@RequestParam(name= "banco") String banco,
+                                 @RequestParam(name = "agencia") String agencia,
+                                 @RequestParam(name = "numero") String numero){
+        return  String.format("Banco: %s, Agencia: %s, Conta: %s. Saldo: R$1500,00", banco, agencia,numero);
+
+    }
 }
+
+// http://localhost:8080/contas?banco=888&agencia=1111&numero=3333
