d1757464eae62443a3a45e48eea8715fb0d8126e
Felipe Ferreira de Paula <felipefp2@gmail.com>
2023-06-21 14:54:27 -0300

    FilmesC#

---
+++ Aulas C#/FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,13 @@
+﻿using FilmesApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers{    [ApiController]    [Route("filme")]    public class FilmeController    {        private static List<Filme> filmes = new List<Filme>();        [HttpPost]        public void AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+        }        [HttpGet]        public IEnumerable<Filme> RecuperarFilmes(int skip, int take)
+        {
+            return filmes.Skip(skip).Take(take);
+        }        [HttpGet("{id}")]        public Filme? RecuperarFilmePorId(int id)
+        {
+            return filmes.FirstOrDefault(filme => filme.Id == id);
+        }    }}
---
+++ Aulas C#/FilmesApi/FilmesApi/Model/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Model
+{
+    public class Filme
+    {
+        [
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "O titulo do Filme é obrigatório")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Gênero do Filme é obrigatório")]
+        [MaxLength(50, ErrorMessage ="O Gênero nao pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O Tempo de Duração do Filme é obrigatório")]
+        [Range(50, 600, ErrorMessage = "O Filme deve ter duraçao minima de 50 min, e maxima de 600 min")]
+        public int Duracao { get; set; }
+
+    }
+}
